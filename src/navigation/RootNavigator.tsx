3616ea6a83aaae57caa9db8468552a2f784efb14
Saidax277 <saidax2005@gmail.com>
2026-02-19 09:24:48 +0000

    Conectandome al git

---
+++ src/navigation/RootNavigator.tsx
@@ -0,0 +1,38 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import HomeScreen from "../screens/HomeScreen";
+import LoginScreen from "../screens/LoginScreen";
+import RegisterScreen from "../screens/RegisterScreen";
+
+import ClienteHomeScreen from "../screens/ClienteHomeScreen";
+import PropertyDetailScreen from "../screens/PropertyDetailScreen";
+import ScheduleScreen from "../screens/ScheduleScreen";
+import ConfirmScreen from "../screens/ConfirmScreen";
+
+export type RootStackParamList = {
+  Home: undefined;
+  Login: undefined;
+  Register: undefined;
+
+  ClienteHome: undefined;
+  PropertyDetail: { propiedad: any };
+  Schedule: { propiedad: any };
+  Confirm: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+export default function RootNavigator() {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Register" component={RegisterScreen} />
+
+      <Stack.Screen name="ClienteHome" component={ClienteHomeScreen} />
+      <Stack.Screen name="PropertyDetail" component={PropertyDetailScreen} />
+      <Stack.Screen name="Schedule" component={ScheduleScreen} />
+      <Stack.Screen name="Confirm" component={ConfirmScreen} />
+    </Stack.Navigator>
+  );
+}
---
+++ src/screens/LoginScreen.tsx
@@ -0,0 +1,117 @@
+import { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  ImageBackground,
+} from "react-native";
+
+import { useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { RootStackParamList } from "../navigation/RootNavigator";
+
+type NavProp = NativeStackNavigationProp<RootStackParamList>;
+
+export default function LoginScreen() {
+  const navigation = useNavigation<NavProp>(); // 👉 TIPADO
+
+  const [usuario, setUsuario] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <ImageBackground
+      source={require("../../assets/FondoInicioDos.png")}
+      style={styles.background}
+    >
+      <View style={styles.container}>
+
+        <TextInput
+          style={styles.input}
+          placeholder="Usuario"
+          placeholderTextColor="#666"
+          value={usuario}
+          onChangeText={setUsuario}
+        />
+
+        <TextInput
+          style={styles.input}
+          placeholder="Contraseña"
+          placeholderTextColor="#666"
+          secureTextEntry
+          value={password}
+          onChangeText={setPassword}
+        />
+
+        <View style={styles.linksRow}>
+          <TouchableOpacity onPress={() => navigation.navigate("Register")}>
+            <Text style={styles.linkText}>Registrarse</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity>
+            <Text style={styles.linkText}>¿Olvidó Contraseña?</Text>
+          </TouchableOpacity>
+        </View>
+
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.navigate("ClienteHome")}
+
+        >
+          <Text style={styles.buttonText}>Entrar</Text>
+        </TouchableOpacity>
+
+      </View>
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+  },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingHorizontal: 30,
+    paddingTop: 150,
+  },
+  input: {
+    width: "100%",
+    backgroundColor: "rgba(255,255,255,0.9)",
+    borderRadius: 10,
+    paddingVertical: 12,
+    paddingHorizontal: 15,
+    marginBottom: 15,
+    fontSize: 16,
+    borderColor: "#00A86B",
+    borderWidth: 4,
+  },
+  linksRow: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 25,
+  },
+  linkText: {
+    color: "#00A86B",
+    fontSize: 14,
+    fontWeight: "600",
+  },
+  button: {
+    width: "100%",
+    backgroundColor: "#00A86B",
+    paddingVertical: 14,
+    borderRadius: 10,
+  },
+  buttonText: {
+    color: "#fff",
+    textAlign: "center",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+});
